e830022a6d7282a54b02eb146d738736c7e8b369
RiojasMx <cesar.riojas@hotmail.com>
2026-03-19 07:51:51 -0600

    scaffolding

--- portal-sma-admin/src/app/app.component.ts
+++ portal-sma-admin/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'portal-sma-admin';
